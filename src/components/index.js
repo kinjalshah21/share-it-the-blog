d8e324e08803f1dcc19d4b10656131ef56ccaebe
Kinjal_Shah <kinjalshah83415@gmail.com>
2023-12-10 15:12:31 +0530

    input and button component added.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,13 @@
 import { Header } from "./Header/Header";
-import { Footer } from "./Footer/Footer";
+import {Footer} from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export {Header,Footer}
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
